d9ca3f139f6f465e73bcfc68131db23e77196562
nasir <nasirkhimani@gmail.com>
2025-10-27 15:05:49 -0400

    1.0.5

--- src/ihp/install.sh
+++ src/ihp/install.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-# nix profile add nixpkgs#direnv
-nix profile add nixpkgs#cachix
+echo "=== Setting up cachix ==="
 cachix use cachix
 cachix use digitallyinduced
-mkdir -p ~/.config/direnv && echo '[whitelist]\nprefix = ["/workspaces/"]' > ~/.config/direnv/direnv.toml
-# echo '\\neval \"$(direnv hook bash)\"' >> ~/.bashrc
-echo 'export IHP_BASEURL=$(if [ -n \"${CODESPACE_NAME}\" ]; then echo \"https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}\"; else echo \"http://localhost:8000\"; fi)' >> ~/.bashrc
-echo 'export IHP_IDE_BASEURL=$(if [ -n \"${CODESPACE_NAME}\" ]; then echo \"https://${CODESPACE_NAME}-8001.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}\"; else echo \"http://localhost:8001\"; fi)' >> ~/.bashrc
-echo "prefetching..."
-nix build --accept-flake-config --impure github:digitallyinduced/ihp-boilerplate#devShells.x86_64-linux.default --no-link
 
+echo "=== Installing direnv ==="
+sudo apt update
+sudo apt install -y direnv
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
+
+echo "=== Setting IHP BASEURL ==="
+echo 'export IHP_BASEURL=$(if [ -n "${CODESPACE_NAME}" ]; then echo "https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"; else echo "http://localhost:8000"; fi)' >> ~/.bashrc
+
+echo "=== Setting IHP IDE BASEURL ==="
+echo 'export IHP_IDE_BASEURL=$(if [ -n "${CODESPACE_NAME}" ]; then echo "https://${CODESPACE_NAME}-8001.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"; else echo "http://localhost:8001"; fi)' >> ~/.bashrc
+
+echo "=== Fixing Nix permissions ==="
+sudo apt install -y acl
+sudo setfacl -k /tmp
+
+echo "=== Allowing direnv for /workspaces ==="
+mkdir -p ~/.config/direnv
+touch ~/.config/direnv/direnv.toml
+echo "[whitelist]
+prefix = ['/workspaces/']" >> ~/.config/direnv/direnv.toml
+
+echo "=== Prefetching IHP flake ==="
+nix build --accept-flake-config --impure github:digitallyinduced/ihp-boilerplate#devShells.x86_64-linux.default --no-link
